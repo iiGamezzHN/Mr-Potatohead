7fdfd377a224e03e202c30fe3236dc645cbb6819
David Arisz <davidarisz@gmail.com>
2018-11-01 22:02:23 +0100

    Implement proper memory management :)

--- app/src/main/java/com/example/davidarisz/mrpotatohead/MainActivity.java
+++ app/src/main/java/com/example/davidarisz/mrpotatohead/MainActivity.java
@@ -2,16 +2,44 @@ package com.example.davidarisz.mrpotatohead;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.GridLayout;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private final static String boxesTag = "boxes";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState != null) {
+            ArrayList<String> boxes = savedInstanceState.getStringArrayList(boxesTag);
+            assert boxes != null;
+            for (String part : boxes) {
+                toggleVisibility(part, false);
+            }
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        GridLayout layout = findViewById(R.id.gridLayout);
+
+        ArrayList<String> boxes = new ArrayList<>();
+
+        for(int i = 0; i < layout.getChildCount(); i++) {
+            CheckBox checkbox = (CheckBox) layout.getChildAt(i);
+            if (checkbox.isChecked()) {
+                boxes.add(checkbox.getText().toString());
+            }
+        }
+
+        savedInstanceState.putStringArrayList(boxesTag, boxes);
+        super.onSaveInstanceState(savedInstanceState);
     }
 
     public void toggle(View view) {
