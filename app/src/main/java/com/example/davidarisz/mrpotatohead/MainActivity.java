be2ee58a5a3e4cde494e305d1a97a92aa4956b52
David Arisz <davidarisz@gmail.com>
2018-11-01 20:52:19 +0100

    Finished the app's logic.

--- app/src/main/java/com/example/davidarisz/mrpotatohead/MainActivity.java
+++ app/src/main/java/com/example/davidarisz/mrpotatohead/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.davidarisz.mrpotatohead;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.CheckBox;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void toggle(View view) {
+        // Cast generic view to CheckBox
+        CheckBox checkbox = (CheckBox) view;
+        String part = checkbox.getText().toString();
+        toggleVisibility(part, !checkbox.isChecked());
+    }
+
+    private void toggleVisibility(String part, boolean visible) {
+        View potatoHead = findViewById(R.id.potatohead_layout);
+        View bodyPart = potatoHead.findViewWithTag(part);
+        if (visible)
+            bodyPart.setVisibility(View.INVISIBLE);
+        else
+            bodyPart.setVisibility(View.VISIBLE);
     }
 }
