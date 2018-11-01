6f9a5c168b90146eabffbb79858b0ac1c5a45a98
David Arisz <davidarisz@gmail.com>
2018-11-01 22:16:33 +0100

    JavaDoc

--- app/src/main/java/com/example/davidarisz/mrpotatohead/MainActivity.java
+++ app/src/main/java/com/example/davidarisz/mrpotatohead/MainActivity.java
@@ -11,6 +11,12 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
     private final static String boxesTag = "boxes";
 
+    /**
+     * This method is called by the Android system when the activity is created.
+     * @param savedInstanceState argument that (might) contain stuff we need to
+     *                           properly re-create our activity
+     *                           (toggle the visibility, for example).
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,17 +24,23 @@ public class MainActivity extends AppCompatActivity {
 
         if (savedInstanceState != null) {
             ArrayList<String> boxes = savedInstanceState.getStringArrayList(boxesTag);
-            assert boxes != null;
+            assert boxes != null; // No bugging notification about possible NullPointerException
             for (String part : boxes) {
+                // Toggle visibility manually
                 toggleVisibility(part, false);
             }
         }
     }
 
+    /**
+     * Method that is called when the Android system decides to suspend the app.
+     * We then quickly save a list of checked checkboxes so that we can toggle the corresponding
+     * images in {@link #onCreate{Bundle}.
+     * @param savedInstanceState see {@link #onCreate(Bundle)}
+     */
     @Override
     public void onSaveInstanceState(Bundle savedInstanceState) {
         GridLayout layout = findViewById(R.id.gridLayout);
-
         ArrayList<String> boxes = new ArrayList<>();
 
         for(int i = 0; i < layout.getChildCount(); i++) {
@@ -42,6 +54,12 @@ public class MainActivity extends AppCompatActivity {
         super.onSaveInstanceState(savedInstanceState);
     }
 
+    /**
+     * This method is called by checking a checkbox.
+     * It extracts the name of the part of which the visibility
+     * has to be toggled and delegates this to {@link #toggleVisibility}.
+     * @param view Argument given by Android.
+     */
     public void toggle(View view) {
         // Cast generic view to CheckBox
         CheckBox checkbox = (CheckBox) view;
@@ -49,6 +67,11 @@ public class MainActivity extends AppCompatActivity {
         toggleVisibility(part, !checkbox.isChecked());
     }
 
+    /**
+     * This (private) method handles the actual toggling of the visibility of the image.
+     * @param part Part of Mr. Potatohead to toggle.
+     * @param visible Whether part is visible now.
+     */
     private void toggleVisibility(String part, boolean visible) {
         View potatoHead = findViewById(R.id.potatohead_layout);
         View bodyPart = potatoHead.findViewWithTag(part);
